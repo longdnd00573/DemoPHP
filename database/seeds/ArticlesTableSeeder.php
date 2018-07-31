693724b8143fdeb5f99e5370636cc5ca2fc9b196
longdnd00573 <longdnd00573@fpt.edu.vn>
2018-07-31 11:54:37 +0700

    Okie

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,156 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::table('articles')->truncate();
+        \Illuminate\Support\Facades\DB::table('articles')->insert([
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay mua',
+                'author'=>'Thao',
+                'content'=>'Banh ngon binh thuong',
+                'images'=>'https://afamilycdn.com/L9AscbailKNM6qlDjYJqDI39UnIrac/Image/2016/03/xac-phat-them-voi-10-mon-banh-dan-da-ngon-lanh-cua-duong-pho-sai-gon_ab77e8b167.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay nang',
+                'author'=>'Thao',
+                'content'=>'Banh rat ngon',
+                'images'=>'https://c2.staticflickr.com/6/5825/22970996514_a721f12743.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay ram',
+                'author'=>'Thao',
+                'content'=>'Banh kha ngon',
+                'images'=>'https://daylambanh.r.worldssl.net/images/kienthuckynang/banh-mi-bread.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'http://quananminhtam.com:7787/mediaroot/media/userfiles/useruploads/563/image/THUC%20AN%20NHANH/banh%20mi%20op%20la%20cha%20lua.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'http://thegioinuocep.com/wp-content/uploads/2016/04/bm1.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon lam',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ],
+            [
+                'name'=>'Banh ngon ngay tet',
+                'author'=>'Thao',
+                'content'=>'Banh ngon',
+                'images'=>'https://baomoi-photo-1-td.zadn.vn/w700_r1/16/11/13/61/20818277/1_58205.jpg'
+            ]
+        ]);
+    }
+}
---
+++ resources/views/admin/article/list.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+<h1>LIST PRODUCT</h1>
+<ul>
+    @foreach($list_obj as $item)
+           <div class="col-3">
+               <h6 class="text-center" href="/admin/article/{{$item -> id}}">{{$item -> name}}</h6>
+               <img class="item1" width="350px" height="200px" src="{{ $item -> images }}" alt="">
+               <div class="item2">
+                   <a class="text-center" href="/admin/article/{{$item -> id}}/edit">Edit</a>&nbsp;&nbsp;
+                   <a  href="#/admin/article/{{$item -> id}}" id="{{$item -> id}}" class="btn-delete">Delete</a>
+               </div>
+           </div>
+    @endforeach
+</ul>
+<script>
+    $('.btn-delete').click(function () {
+        var cateId = this.id;
+        var user_confirm = confirm('Are you sure?');
+        if (user_confirm) {
+            $.ajax({
+                url: 'http://127.0.0.1:8000/admin/article/' + cateId,
+                method: 'DELETE',
+                data: {
+                    '_token':"{{csrf_token()}}"
+                },
+                success: function () {
+                    alert('Success');
+                    window.location.reload();
+                },
+                error: function () {
+                    alert('error');
+                }
+            });
+        } else {
+            alert('!Okie');
+        }
+    });
+</script>
+</body>
+</html>
